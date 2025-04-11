4e6720f98ec84927bf4e30e5a02271b8bd868af4
wesley <wesley@bitechular.com>
2025-04-11 14:33:35 +0200

    Fix librelinkclient dependency

--- backend/src/librelink.rs
+++ backend/src/librelink.rs
@@ -25,34 +25,40 @@ impl LibreLinkSyncRuntime {
 
         loop {
             let measurement = get_latest_measurement(&self.libre_link).await;
-            if let Ok(measurement) = measurement {
-                if self
-                    .last_date
-                    .clone()
-                    .map(|ld| !ld.eq(&measurement.time))
-                    .unwrap_or(true)
-                {
-                    match save_measurement(&self.influxdb, &measurement).await {
-                        Err(e) => {
-                            log::error!("Failed to save measurement to influxdb: {}", e);
-                        }
-                        Ok(_) => {
-                            log::debug!(
+            match measurement {
+                Ok(measurement) =>{
+                    if self
+                        .last_date
+                        .clone()
+                        .map(|ld| !ld.eq(&measurement.time))
+                        .unwrap_or(true)
+                    {
+                        match save_measurement(&self.influxdb, &measurement).await {
+                            Err(e) => {
+                                log::error!("Failed to save measurement to influxdb: {}", e);
+                            }
+                            Ok(_) => {
+                                log::debug!(
                                 "Saved {} @ {} to influxdb",
                                 &measurement.value,
                                 &measurement.time
                             );
-                            self.last_date = Some(measurement.time);
+                                self.last_date = Some(measurement.time);
+                            }
                         }
-                    }
-                } else {
-                    log::trace!(
+                    } else {
+                        log::trace!(
                         "Skipping measurement @ {} because it was not updated ({:?})",
                         measurement.time,
                         self.last_date
                     );
+                    }
+                }
+                Err(err) => {
+                    log::error!("Failed to get measurement from librelink: {}", err);
                 }
             }
+
             tokio::time::sleep(tokio::time::Duration::from_secs(30)).await;
         }
     }
@@ -73,7 +79,6 @@ pub async fn get_latest_measurement(client: &LibreLinkClient) -> Result<GlucoseM
             "%m/%d/%Y %I:%M:%S %p",
         )?;
         let datetime_utc: DateTime<Utc> = Utc.from_utc_datetime(&naive_datetime);
-
         return Ok(GlucoseMeasurement {
             time: datetime_utc.fixed_offset().to_utc(),
             value: round_value(conn.glucose_measurement.value, 2),
